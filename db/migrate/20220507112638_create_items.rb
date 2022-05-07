b7788550c70333c90853bfcab3b6184a040b3ba6
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-07 20:56:25 +0900

    Userモデル、データベースの作成

---
+++ db/migrate/20220507112638_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string      :title
+      t.text        :explanation
+      t.integer     :category_id
+      t.integer     :status_id
+      t.integer     :shipping_fee_id
+      t.integer     :prefecture_id
+      t.integer     :send_day_id
+      t.integer     :price
+      t.references  :user,  null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
