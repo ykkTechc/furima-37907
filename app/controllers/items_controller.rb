62f3b871f1771f37033c839aeaf8d6d57c702aa4
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-09 14:15:02 +0900

    コメントアウト解除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,35 +1,29 @@
-# class ItemsController < ApplicationController
-#   before_action :authenticate_user!, except:[:index]
-#  
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, except:[:index]
+ 
 
-#   def index
-#     @items = Item.includes(:user)
-#   end
+  def index
+    @items = Item.includes(:user)
+  end
   
-#   def new
-#     @item = Item.new
-#   end
+  def new
+    @item = Item.new
+  end
 
-#   def create
-#     @item = Item.new(item_params) 
-#     if @item.save
-#       redirect_to root_path
-#     else
-#       render :new
-#     end
-#   end
+  def create
+    @item = Item.new(item_params) 
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-#   private
+  private
 
-#  def item_params
-#     items.require(:item).permit(:title, :explanation, :category_id, :status_id, :sipping_fee_id, :prefecture_id, :send_day_id, :price, :image)
-#  end
+ def item_params
+    items.require(:item).permit(:title, :explanation, :category_id, :status_id, :sipping_fee_id, :prefecture_id, :send_day_id, :price, :image)
+ end
 
+end
 
-
-
-
-
-
-  
-# end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,17 +1,17 @@
-# class Item < ApplicationRecord
-#   extend ActiveHash::Associations::ActiveRecordExtensions
-#   belongs_to :category
-#   belongs_to :prefecture
-#   belongs_to :send_day
-#   belongs_to :sipping_fee
-#   belongs_to :status
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :send_day
+  belongs_to :sipping_fee
+  belongs_to :status
 
-#   has_one_attached :image
+  has_one_attached :image
 
 
-#   validates :category,    numericality: { other_than: 1 , message: "can't be blank"} 
-#   validates :prefecture,  numericality: { other_than: 1 , message: "can't be blank"}
-#   validates :send_day,    numericality: { other_than: 1 , message: "can't be blank"}
-#   validates :sipping_fee, numericality: { other_than: 1 , message: "can't be blank"}
-#   validates :status,      numericality: { other_than: 1 , message: "can't be blank"}
-# end
+  validates :category,    numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :send_day,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sipping_fee, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status,      numericality: { other_than: 1 , message: "can't be blank"}
+end
