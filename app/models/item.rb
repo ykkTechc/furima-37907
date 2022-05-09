8133ff7dcf05afca19cd4d7f4c6d94444caa9a72
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-09 16:04:58 +0900

    出品機能に関するコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,10 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :prefecture
   belongs_to :send_day
-  belongs_to :sipping_fee
-  belongs_to :status
+  # belongs_to :sipping_fee
+  # belongs_to :status
 
-  has_one_attached :image
+  # has_one_attached :image
 
 
   # validates :category,    numericality: { other_than: 1 , message: "can't be blank"} 
