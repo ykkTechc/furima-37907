aa619fb2fa9a7e58cda41e842c81fa1969bb204a
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-09 20:48:55 +0900

    販売価格の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,19 @@
 window/addEventListener('load', () => {
-  
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+   
+
+    const addTaxDom  = document.getElementById("add-tax-price");
+    add_value =(inputValue * 0.1)
+    addTaxDom.innerHTML = Math.floor(add_value)
+    console.log(add_value);
+    
+    
+    const profitNumber = document.getElementById("profit")
+    profitNumber.innerHTML = Math.floor(inputValue - add_value)
+    console.log(profitNumber);
+    
+  })
 });
