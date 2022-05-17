842d326ff1f10b00934edba9c63ea351008c7b8f
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-05-17 11:44:53 +0900

    Revert "商品購入機能実装（テストは未実施）"
    This reverts commit 1c2206f9244272bc21893e0d08dbd08542b9a336.

--- app/models/order.rb
+++
@@ -1,6 +0,0 @@
-class Order < ApplicationRecord
-  
-  belongs_to :user
-  belongs_to :item
-  has_one    :delivery
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
-  resources :orders, only: [:index, :new, :create]
-  end
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
 end
